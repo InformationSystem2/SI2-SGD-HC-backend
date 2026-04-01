41ce62e1119d929534f67b3e25a43286589d1370
evert.aov <evert.aov@gmail.com>
2026-04-01 14:41:30 -0400

    feat: Add methods to UserRepository for document number and username handling

--- src/main/java/com/sgd_hc/sgd_hc/module_users/repository/UserRepository.java
+++ src/main/java/com/sgd_hc/sgd_hc/module_users/repository/UserRepository.java
@@ -8,10 +8,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sgd_hc.sgd_hc.module_users.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByCi(String ci);
     Optional<User> findByEmail(String email);
-    boolean existsByCi(String ci);
     boolean existsByEmail(String email);
-    List<User> findAllByCi(String ci);
     List<User> findAllByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    Optional<User> findByDocumentNumber(String documentNumber);
+    boolean existsByDocumentNumber(String documentNumber);
 }
