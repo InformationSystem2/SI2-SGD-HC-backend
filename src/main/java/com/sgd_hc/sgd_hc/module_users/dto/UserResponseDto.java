94dff5e8df58a4ae0669d7de28ce1e9b4209b7f7
evert.aov <evert.aov@gmail.com>
2026-04-01 14:41:36 -0400

    feat: Update UserResponseDto to include username, phone, document type, document number, and gender fields

--- src/main/java/com/sgd_hc/sgd_hc/module_users/dto/UserResponseDto.java
+++ src/main/java/com/sgd_hc/sgd_hc/module_users/dto/UserResponseDto.java
@@ -3,13 +3,17 @@ package com.sgd_hc.sgd_hc.module_users.dto;
 import java.util.Set;
 
 public record UserResponseDto(
-    Long id,
-    String ci,
-    String email,
-    String firstName,
-    String lastName,
-    Boolean isActive,
-    Set<Long> rolesIds
+        Long id,
+        String username,
+        String email,
+        String firstName,
+        String lastName,
+        String phone,
+        String documentType,
+        String documentNumber,
+        Boolean gender,
+        String isActive,
+        Set<Long> rolesIds
 ) {
 
 }
