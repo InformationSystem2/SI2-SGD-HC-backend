194bb98b59993254041876037182719bfdc36f85
evert.aov <evert.aov@gmail.com>
2026-04-06 21:15:50 -0400

    feat: Add custom query method to UserRepository for enhanced user retrieval

--- src/main/java/com/sgd_hc/sgd_hc/module_users/repository/UserRepository.java
+++ src/main/java/com/sgd_hc/sgd_hc/module_users/repository/UserRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sgd_hc.sgd_hc.module_users.entity.User;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
